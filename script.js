8f995d63299efcd5d7016fb12ae36e07e0c9b957
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-01-14 12:13:12 +0200

    .

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('AAA');
+replaceText(document.body);
+
+function replaceText(element) {
+    if (element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText)
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/covid-19/gi, 'TEXT')
+    }
+}
