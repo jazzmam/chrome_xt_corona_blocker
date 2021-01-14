762780749fde2123502c97808e1cc384f8351e7f
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-01-14 13:07:31 +0200

    .

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        element.textContent = element.textContent.replace(/covid-19/gi, 'TEXT')
+        if (element.textContent.match(/covid-19|coronavirus|koronavir|corona/gi)) {
+            element.parentElement.remove();
+        }
+        /*element.textContent = element.textContent.replace(/covid-19|coronavirus|koronavir|corona/gi, 'TEXT')*/
     }
 }
