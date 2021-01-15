6db73a8c404a03195c96c61df1f21c1e1be1b413
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-01-15 11:46:29 +0200

    .

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        if (element.textContent.match(/covid-19|coronavirus|koronavir|corona/gi)) {
+        if (element.textContent.match(/covid-19|coronavirus|koronavir|pendem|corona/gi)) {
             element.parentElement.remove();
         }
     }
 }
 
 // Floating button
-let div = document.createElement("div");
-div.classList.add('floating-btn-switch');
-div.id = "floating-btn-switch";
+let divButton = document.createElement("div");
+divButton.classList.add('floating-btn-switch');
+divButton.id = "floating-btn-switch";
 
+// Image inside the floating button
 let img = document.createElement("img");
-img.src = 'https://i.pinimg.com/originals/f0/e1/8a/f0e18a5eec5f034b4bf92ea25ccc7d48.png';
+img.src = 'https://londonvetspecialists.vet/wp-content/uploads/2020/04/covid-icon.png';
 
-document.body.appendChild(div);
-div.appendChild(img);
+
+
+document.body.appendChild(divButton);
+divButton.appendChild(img);
