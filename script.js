eb8619c871fe25ef714ba2cf993989364c6c7fa8
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-01-19 10:46:00 +0200

    .

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 
-// Hiding text that is covid-related
-function hideText(element) {
-
-    // Hiding stroke line of the button
-    document.getElementById("button-stroke").style.display = 'none';
-
-    if (element.hasChildNodes()) {
-        element.childNodes.forEach(hideText)
-    } else if (element.nodeType === Text.TEXT_NODE) {
-        if (element.textContent.match(/covid-19|coronavirus|koronavir|pendem|corona/gi)) {
-            element.parentElement.remove();
-        }
-    }
-}
-
-function changeArticlesDisplay() {
-    // if statment here
-
-    hideText(document.body);
-}
-
 // Floating button
 let divButton = document.createElement("div");
 divButton.classList.add('floating-btn-switch');
@@ -38,5 +17,45 @@ document.body.appendChild(divButton);
 divButton.appendChild(img);
 divButton.appendChild(divStroke);
 
+let areHidden = false;
 
+// Hiding text that is covid-related
+function hideText(element) {
+
+	// Hiding stroke line of the button
+	document.getElementById("button-stroke").style.display = 'none';
+
+	// Hiding text that is covid-related
+	if (element.hasChildNodes()) {
+		element.childNodes.forEach(hideText)
+	} else if (element.nodeType === Text.TEXT_NODE) {
+		if (element.textContent.match(/covid-19|coronavirus|koronavir|pendem|corona/gi)) {
+			element.parentElement.remove();
+		}
+	}
+}
+
+function showArticles(element) {
+	console.log("inside showArticles function ");
+}
+
+function changeArticlesDisplay() {
+	// IF STATEMENT HERE
+
+	if (areHidden) {
+		areHidden = false;
+		// DELETE CONSOLE LATER ONE
+		console.log("areHidden ", areHidden);
+
+		showArticles(document.body);
+	} else {
+		areHidden = true;
+		// DELETE CONSOLE LATER ONE
+		console.log("areHidden ", areHidden);
+	
+		hideText(document.body);
+	}
+}
+
+// Event listener
 document.getElementById("floating-btn-switch").addEventListener("click", changeArticlesDisplay);
